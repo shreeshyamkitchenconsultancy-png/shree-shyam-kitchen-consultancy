61439cfc77d2662fb8ddf49d8f88e430503faf57
shreeshyamkitchenconsultancy-png <shreeshyamkitchenconsultancy@gmail.com>
2026-07-09 19:57:40 +0530

    change frequency to daily

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://shreeshyamkitchenconsultancy.com',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
   ]
